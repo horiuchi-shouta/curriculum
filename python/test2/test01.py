4ee6c152a002a29483afe23ca1a767faf341ec5f
horiuchi-shouta <108608319+horiuchi-shouta@users.noreply.github.com>
2022-10-18 23:31:14 +0900

    課題提出

--- python/test2/test01.py
+++ python/test2/test01.py
@@ -11,11 +11,10 @@ while True:  # 無限ループ
         print('数字以外が入力されました。数字のみを入力してください')
 
         continue
-    if answer< number:
+    if answer< number:   #10<number
         print('もっと小さな数値です')
-        # 問②：画像のように表示するように、コードを記述してください。
-    elif answer<8:
-        print('もっと大きな数字を入力して下さい。')
+    elif number < answer: # number<10
+        print('もっと大きな数値です')
     else:
         break  # 変数answerの値と変数numberの値が等しければ終了
 
