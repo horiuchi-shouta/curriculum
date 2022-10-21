39e9b4f9cf144251700f1eaba5c2c0d94f92dad4
horiuchi-shouta <108608319+horiuchi-shouta@users.noreply.github.com>
2022-10-21 20:05:52 +0900

    課題提出

--- python/test2/test02.py
+++ python/test2/test02.py
@@ -5,33 +5,47 @@ while True:
         print('2: IndexError例外を発生')
         print('3: 例外を発生させない')
         print('4: 終了')
-        number = int(input('選択してください。: '))
         #input関数とは、ユーザーがキーボードに入力したデータを受け付けるための関数
-        
-        
-    except ValueError as e:
-        print(e.args)
-        continue
+        number = int(input('選択してください。: '))
 
         # 問①：if文を用いて、以下の画像のように処理されるように記述しましょう。
-    if number==4:
-        print('終了します')
-        break
-
-    elif number==1:
-        
+        if number==1:    
+            tmp=int('hoge')
+            continue
+        if number==2:
+            s=[10,20,30]
+            print(s[100])
+            continue
+        if number==3:
+            print('↓')
+            print('例外を発生させませんでした')
+            print('↓')
+            print('もう一度選択しましょう')
+            continue
+         
+    except ValueError as e:
         print('↓')
         print('ValueError')
         print(e.args)
         print('↓')
         print('もう一度選択しましょう')
+        continue
 
-    else:
-        print('１以外')
-    
+    except IndexError as e:
+        print('↓')
+        print('IndexError')
+        print(e.args)
+        print('↓')
+        print('もう一度選択しましょう')
+        continue
 
     # 問②：else節を用いて、以下の画像のように処理されるように記述しましょう。
-    ##ここに書く
+    else:
+        print('↓')
+        print('終了します')
+        break
+                
 
+    
 print('↓')
 print('無限ループを終了します')
